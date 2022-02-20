56a0d9765faab9a24d02f17744145109df77857c
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-02-20 13:01:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@ pacman -S --needed --noconfirm archiso mkinitcpio-archiso
 pacman -S wget --noconfirm --needed
 wget https://raw.githubusercontent.com/endeavouros-team/PKGBUILDS/master/endeavouros-mirrorlist/endeavouros-mirrorlist -O /etc/pacman.d/endeavouros-mirrorlist
 wget https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/airootfs/etc/pacman.conf -O /etc/pacman.conf
-pacman -S --noconfirm --needed endeavouros-keyring
-pacman-key --init
-pacman-key --populate archlinux endeavouros
-pacman-key --refresh-keys
+sudo pacman-key --keyserver keyserver.ubuntu.com -r 003DB8B0CB23504F
+sudo pacman-key --lsign 003DB8B0CB23504F
 
 mkarchiso -v -w ./work -o ./out ./
 echo "ISO Built in ./out folder!"
