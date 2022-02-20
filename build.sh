5318b73c59f1a5af5d7883996429cc78e5032448
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-02-20 13:49:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 pacman -S --noconfirm --needed archlinux-keyring
 pacman -S --needed --noconfirm archiso mkinitcpio-archiso
 pacman -S wget --noconfirm --needed
-pacman-key --init && pacman-key --recv-key 003DB8B0CB23504F --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 003DB8B0CB23504F && pacman --noconfirm -U 'https://mirror.alpix.eu/endeavouros/repo/endeavouros/x86_64/endeavouros-'{keyring,mirrorlist}'.pkg.tar.zst'
-echo "[multilib]" >> /etc/pacman.conf && echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf && echo "[endeavouros]" >> /etc/pacman.conf && echo "SigLevel = PackageRequired" >> /etc/pacman.conf && echo "Include = /etc/pacman.d/endeavouros-mirrorlist" >> /etc/pacman.conf
+pacman-key --init && pacman-key --recv-key 003DB8B0CB23504F --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 003DB8B0CB23504F
+wget https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/airootfs/etc/pacman.conf -O /etc
+wget https://raw.githubusercontent.com/endeavouros-team/PKGBUILDS/master/endeavouros-mirrorlist/endeavouros-mirrorlist -O /etc/pacman.d/endeavouros-mirrorlist
+pacman -S --noconfirm --needed endeavouros-keyring endeavouros-mirrorlist
 
 mkarchiso -v -w ./work -o ./out ./
 echo "ISO Built in ./out folder!"
