1fc4303b09265eb0b91b7b9b62eae40eebdb1c6c
keybreak <47599989+keybreak@users.noreply.github.com>
2022-05-03 14:45:26 +0300

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -19,8 +19,8 @@ airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' 
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
-  ["/root"]="0:0:700"
+  ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
-  ["/etc/sudoers.d"]="0:0:755"
+  ["/etc/sudoers.d"]="0:0:750"
   ["/etc/sudoers.d/g_wheel"]="0:0:440"
 )
