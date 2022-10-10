82f15e3f6db560a7e776428492ecba22517165d7
Johannes Kamprad <archlinux@kamprad.net>
2022-10-10 12:33:06 +0200

    Update prepare.sh
    removed:
    ```
    
    # Get default mkinitcpio.conf
    wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/archlinux/mkinitcpio/master/mkinitcpio.conf"
    ```
    not needed for dracut

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 # Get mirrorlist for offline installs
-wget -qN --show-progress -P "airootfs/etc/pacman.d/" "https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/mirrorlist"
-
-# Get default mkinitcpio.conf
-wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/archlinux/mkinitcpio/master/mkinitcpio.conf"
+wget -qN --show-progress -P "airootfs/etc/pacman.d/" "https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/mirrorlist
 
 # Get wallpaper for installed system
 wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/endeavouros-theming/master/backgrounds/endeavouros-wallpaper.png"
