a03bffcd92b2563f2d571da73f680b29c4b96342
killajoe <archlinux@kamprad.net>
2022-12-08 12:45:48 +0100

    adding wallpaper to reset.sh

--- reset.sh
+++ reset.sh
@@ -2,6 +2,7 @@
 rm -rf "work" "out"
 rm airootfs/root/packages/*.pkg.tar.zst
 rm -rf airootfs/root/endeavouros-skel-liveuser/pkg
+rm airootfs/root/endeavouros-wallpaper.png
 rm airootfs/root/endeavouros-skel-liveuser/*.pkg.tar.zst
 rm airootfs/root/endeavouros-skel-liveuser/*.pkg.tar.zst.sig
 rm -rf airootfs/etc/pacman.d/
