8aa801714623504d59347343946108ebb3069b07
killajoe <archlinux@kamprad.net>
2022-12-15 12:01:57 +0100

    fix double line in prepare.sh

--- reset.sh
+++ reset.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 rm -rf "work" "out"
 rm airootfs/root/packages/*.pkg.tar.zst
+rm airootfs/root/packages/*.pkg.tar.zst.sig
 rm -rf airootfs/root/endeavouros-skel-liveuser/pkg
 rm airootfs/root/endeavouros-wallpaper.png
 rm airootfs/root/endeavouros-skel-liveuser/*.pkg.tar.zst
-rm airootfs/root/endeavouros-skel-liveuser/*.pkg.tar.zst.sig
 rm -rf airootfs/etc/pacman.d/
 rm eosiso*.log
