cc59c9005b23e2159367ba1f727e09d575582ca1
killajoe <archlinux@kamprad.net>
2022-12-17 00:53:06 +0100

    fixes for liveuser fro xfce4 18 update

--- prepare.sh
+++ prepare.sh
@@ -7,8 +7,8 @@ wget -qN --show-progress -P "airootfs/etc/pacman.d/" "https://raw.githubusercont
 wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/endeavouros-theming/master/backgrounds/endeavouros-wallpaper.png"
 
 # current downgrade mesa for calamares lag in vms:
-wget "https://archive.archlinux.org/packages/m/mesa/mesa-22.1.7-1-x86_64.pkg.tar.zst"
-mv mesa-22.1.7-1-x86_64.pkg.tar.zst "airootfs/root/packages/"
+#wget "https://archive.archlinux.org/packages/m/mesa/mesa-22.1.7-1-x86_64.pkg.tar.zst"
+#mv mesa-22.1.7-1-x86_64.pkg.tar.zst "airootfs/root/packages/"
 
 # Make sure build scripts are executable
 chmod +x "./"{"mkarchiso","run_before_squashfs.sh"}
