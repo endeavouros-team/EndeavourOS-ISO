a51da0f5ab5c603d6e0fa8682a395cc5035aecae
Johannes Kamprad <archlinux@kamprad.net>
2024-03-10 20:06:05 +0100

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="EndeavourOS_Galileo"
+iso_name="EndeavourOS_Gemini"
 iso_label="EOS_$(date +%Y%m)"
 iso_publisher="EndeavourOS <https://endeavouros.com>"
 iso_application="EndeavourOS Live/Rescue CD"
