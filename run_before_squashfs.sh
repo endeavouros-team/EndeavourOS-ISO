422f848fdc1595f0ebb8087327178ef52e1e80e2
Johannes Kamprad <archlinux@kamprad.net>
2024-06-28 11:30:07 +0200

    [run_before_squashfs.sh] package version file change from nvidia-dkms to nvidia

--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -109,7 +109,7 @@ pacman -Qs | grep "/firefox " | cut -c7- >> iso_package_versions
 pacman -Qs | grep "/linux " | cut -c7- >> iso_package_versions
 pacman -Qs | grep "/mesa " | cut -c7- >> iso_package_versions
 pacman -Qs | grep "/xorg-server " | cut -c7- >> iso_package_versions
-pacman -Qs | grep "/nvidia-dkms " | cut -c7- >> iso_package_versions
+pacman -Qs | grep "/nvidia " | cut -c7- >> iso_package_versions
 mv "iso_package_versions" "/home/liveuser/"
 
 echo "############################"
