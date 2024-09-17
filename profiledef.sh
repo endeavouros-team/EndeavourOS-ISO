8c3f95c06a49269aff26a4ba3efe0569b2bfa080
killajoe <archlinux@kamprad.net>
2024-09-17 15:02:01 +0200

    [profiledef.sh pacman.conf mkinitcpio.conf] changes for pacman 7 update

--- profiledef.sh
+++ profiledef.sh
@@ -14,7 +14,7 @@ out_dir="out"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 
 arch="x86_64"
-pacman_conf="airootfs/etc/pacman.conf"
+pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
