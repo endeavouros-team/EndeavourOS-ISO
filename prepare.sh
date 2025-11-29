de80b0ef9d30c5e2afb2758417a2ef1e95052ad1
Johannes Kamprad <archlinux@kamprad.net>
2025-11-30 00:15:55 +0100

    Uncomment get_pkg function and its call

--- prepare.sh
+++ prepare.sh
@@ -23,12 +23,12 @@ wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/
 # Make sure build scripts are executable
 chmod +x "./"{"mkarchiso","run_before_squashfs.sh"}
 
-#get_pkg() {
-#    sudo pacman -Syw "$1" --noconfirm --cachedir "airootfs/root/packages" \
-#    && sudo chown $USER:$USER "airootfs/root/packages/"*".pkg.tar"*
-#}
+get_pkg() {
+    sudo pacman -Syw "$1" --noconfirm --cachedir "airootfs/root/packages" \
+    && sudo chown $USER:$USER "airootfs/root/packages/"*".pkg.tar"*
+}
 
-#get_pkg "eos-settings-plasma"
+get_pkg "eos-settings-plasma"
 
 # Build liveuser skel
 cd "airootfs/root/endeavouros-skel-liveuser"
