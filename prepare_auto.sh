6ef8f3eb587802ff9ff6aafb02ef24671fdb675b
Johannes Kamprad <archlinux@kamprad.net>
2026-03-02 05:52:37 +0100

    Update prepare_auto.sh to use BUILD_USER variable

---
+++ prepare_auto.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+BUILD_USER="iso-builder-temp"
+
+# add date to wallpaper
+cp airootfs/root/livewall.png airootfs/root/livewall-original.png
+
+magick airootfs/root/livewall.png \
+  -gravity NorthEast \
+  -pointsize 24 \
+  -fill white \
+  -font "DejaVu-Sans" \
+  -annotate +10+10 "$(date '+%Y-%m-%d')" \
+  airootfs/root/livewall.png
+
+# Get wallpaper for installed system
+wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/Branding/master/backgrounds/endeavouros-wallpaper.png"
+
+
+# Make sure build scripts are executable
+chmod +x "./"{"mkarchiso","run_before_squashfs.sh"}
+
+# uncomment to comment calamares package in packages.x86_64 in case you use local build of it
+#[ -n "$(ls airootfs/root/packages/*calamares* 2>/dev/null)" ] && sed -i '/calamares/ s/^/#/' packages.x86_64
+
+
+get_pkg() {
+    mkdir -p /tmp/pkg-cache \
+    && pacman -Syw "$1" --cachedir /tmp/pkg-cache --noconfirm \
+    && cp /tmp/pkg-cache/*.pkg.tar.zst airootfs/root/packages/ \
+    && chown $BUILD_USER:$BUILD_USER "airootfs/root/packages/"*".pkg.tar"*
+}
+
+get_pkg "eos-settings-plasma"
+
+# Build liveuser skel
+cd "airootfs/root/endeavouros-skel-liveuser"
+runuser -u "$BUILD_USER" -- makepkg -s --noconfirm
