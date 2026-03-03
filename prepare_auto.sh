4d245c270b3d063c955d5567029b27f9be0a953d
Johannes Kamprad <archlinux@kamprad.net>
2026-03-03 01:09:34 +0100

    Update prepare_auto.sh

--- prepare_auto.sh
+++ prepare_auto.sh
@@ -15,13 +15,42 @@ magick airootfs/root/livewall.png \
 # Get wallpaper for installed system
 wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/Branding/master/backgrounds/endeavouros-wallpaper.png"
 
-
 # Make sure build scripts are executable
 chmod +x "./"{"mkarchiso","run_before_squashfs.sh"}
 
 # uncomment to comment calamares package in packages.x86_64 in case you use local build of it
 [ -n "$(ls airootfs/root/packages/*calamares* 2>/dev/null)" ] && sed -i '/calamares/ s/^/#/' packages.x86_64
 
+echo "---> generate mirrorlist safely ---> "
+get_country() {
+  for url in \
+    "https://ipapi.co/country_code" \
+    "https://ifconfig.co/country-iso" \
+    "https://ipinfo.io/country"; do
+
+    code="$(curl -fs "$url" 2>/dev/null | grep -oE '^[A-Z]{2}$')"
+    [[ -n "$code" ]] && echo "$code" && return
+  done
+}
+
+COUNTRY="$(get_country)"
+
+if [[ -n "$COUNTRY" ]]; then
+  reflector \
+    --country "$COUNTRY" \
+    --protocol "https" \
+    --sort "rate" \
+    --latest "10" \
+    --save "mirrorlist"
+else
+  reflector \
+    --protocol "https" \
+    --sort "rate" \
+    --latest "20" \
+    --save "/mirrorlist"
+fi
+
+echo "---> generate mirrorlist done ---> "
 
 #get_pkg() {
 #    mkdir -p /tmp/pkg-cache \
